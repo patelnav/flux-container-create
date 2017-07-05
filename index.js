6179a1ddacd6f0477663ab4f5a04ba1bc26a37eb
Nav Patel <nav@abhi.in>
2017-07-05 14:50:55 +0530

    See https://github.com/facebook/flux/issues/351. Fix is based on https://github.com/facebook/flux/issues/351#issuecomment-243175376

---
+++ index.js
@@ -0,0 +1,14 @@
+// Based on https://github.com/facebook/flux/issues/351#issuecomment-243175376
+
+const flux_utils = require('flux/utils')
+
+module.exports = function FluxContainerCreate(containerClass) {
+    const tmp = containerClass;
+    containerClass = function(...args) {
+        return new tmp(...args);
+    };
+    containerClass.prototype = tmp.prototype;
+    containerClass.getStores = tmp.getStores;
+    containerClass.calculateState = tmp.calculateState;
+    return flux_utils.Container.create(containerClass);
+}
